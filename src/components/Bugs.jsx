39a43bc4ce6b977089525a9c6c9d7261bd56c96d
Muhammad A.G. Bello <magbello157@gmail.com>
2021-02-18 15:15:00 +0100

    connect to a react frontend

---
+++ src/components/Bugs.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { loadBugs } from '../store/bugs';
+
+class Bugs extends Component {
+  componentDidMount() {
+    this.props.loadBugs();
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.props.bugs.map(bug => (
+          <li key={bug.id}>{bug.description}</li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+// bugs: state.entities.bugs.list
+const mapStateToProps = state => ({
+  bugs: state.entities.bugs.list
+});
+
+const mapDispatchToProps = dispatch => ({
+  loadBugs: () => dispatch(loadBugs())
+});
+
+// Container
+//  Presentation (Bugs)
+
+export default connect(mapStateToProps, mapDispatchToProps)(Bugs);
